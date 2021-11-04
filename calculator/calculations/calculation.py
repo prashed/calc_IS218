e69568818ab9b9cd783a8a883069c8541efdea73
rashe <pr37@njit.edu>
2021-11-04 19:38:58 -0400

    first commit

---
+++ calculator/calculations/calculation.py
@@ -0,0 +1,8 @@
+""" Calculation component - base of all calculations done """
+
+
+class Calculation:
+    """ Calculation class to be instantiated"""
+
+    def __init__(self, *values):
+        self.values = values
