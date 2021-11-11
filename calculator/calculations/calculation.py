fdb711240f8f2566e5169fe2024236e9dd920c0e
rashe <pr37@njit.edu>
2021-11-11 15:47:07 -0500

    removed

--- calculator/calculations/calculation.py
+++
@@ -1,8 +0,0 @@
-""" Calculation component - base of all calculations done """
-
-
-class Calculation:  # pylint: disable=too-few-public-methods
-    """ Calculation class to be instantiated"""
-
-    def __init__(self, *values):
-        self.values = values
